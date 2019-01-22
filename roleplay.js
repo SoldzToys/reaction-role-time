5449814ecd33bbd07169cb92a84a69c1a2c52405
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2019-01-22 14:59:10 -0500

    Update roleplay.js

--- roleplay.js
+++ roleplay.js
@@ -44,24 +44,34 @@ await reacttime.react(message.guild.emojis.get('461698993261510676'))
 message.delete()
 }
   
- if (message.content.startsWith(`${prefix}react`)) {
-//    let args = message.content.split(/ +/g).slice(1) 
-//   // let reaction = args.slice(1)
-//   // if (!reaction) return message.channel.send("Insert a message ID")
-//    if (!args) return message.channel.send("Insert a message ID")
-//    let reactionthis = args.slice(1)
-//    let channel = message.guild.channels.find(c => c.id === '534561180811919360');
-//    channel.fetchMessage(args[0])
-//   re.react('⭐')
-//    .catch(console.error);
-//    console.log(`Reacted to ${args}!`) 
-//   //.catch(e => console.log(e));
+//  if (message.content.startsWith(`${prefix}react`)) {
+// //    let args = message.content.split(/ +/g).slice(1) 
+// //   // let reaction = args.slice(1)
+// //   // if (!reaction) return message.channel.send("Insert a message ID")
+// //    if (!args) return message.channel.send("Insert a message ID")
+// //    let reactionthis = args.slice(1)
+// //    let channel = message.guild.channels.find(c => c.id === '534561180811919360');
+// //    channel.fetchMessage(args[0])
+// //   re.react('⭐')
+// //    .catch(console.error);
+// //    console.log(`Reacted to ${args}!`) 
+// //   //.catch(e => console.log(e));
+//  }
+  
+   if (message.content.startsWith(`${prefix}react`)) {
+ let args = message.content.slice(1).split(" "); 
+ let reaction = args.slice(1)
+ if (!reaction) return message.channel.send("Insert a message ID")
+   let channel = message.guild.channels.find(c => c.id === '534561180811919360');
+ channel.fetchMessage(args[0]).then(messages => {
+    message.channel.fetchMessage(messages.array()[1].id).react(args[1])
+ //.react(args[1])
+ .catch(console.error);
+ console.log(`Reacted to ${args}!`) 
+ .catch(e => console.log(e));
+ })
  }
-  if (message.content.startsWith(`${prefix}actuallyworking`)) {
-    message.channel.fetchMessages({limit:2}).then(messages => {
-    message.channel.fetchMessage(messages.array()[1].id).react('⭐')
-})
-}
+
 });
 
 client.on("messageReactionAdd", async (reaction, user) => {
