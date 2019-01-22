4289b9f5869d1f1d6adc0b346537b6687177bbe8
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2019-01-22 16:26:55 -0500

    Update roleplay.js

--- roleplay.js
+++ roleplay.js
@@ -62,12 +62,12 @@ message.delete()
 let args = message.content.split(/ +/g).slice(1)
     let reaction = args[0]
    if (!reaction) return message.channel.send("Insert a message ID")
-//    let emojis = args.slice(1).join(" ");
-//    if (!emojis) return message.channel.send("No emoji picked?! Try again.")
-      let reactie = args.slice(1).join(" ");
-   if (!reactie) return message.channel.send("No emoji picked?! Try again.")
- //  let emojipick = client.emojis.find(emoji => emoji.name === emojis)
-   let emojipick = message.guild.emojis.get(reactie)
+   let emojis = args.slice(1).join(" ");
+   if (!emojis) return message.channel.send("No emoji picked?! Try again.")
+//       let reactie = args.slice(1).join(" ");
+//    if (!reactie) return message.channel.send("No emoji picked?! Try again.")
+  let emojipick = client.emojis.find(emoji => emoji.name === emojis)
+//    let emojipick = message.guild.emojis.get(reactie)
    if (!emojipick) return message.channel.send("No such emoji is there.")
    let channel = message.guild.channels.find(c => c.id === '534561180811919360');
 let fetchedMessage = await channel.fetchMessage(reaction)
