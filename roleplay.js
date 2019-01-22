e280f9e53f5d06cc02c9440b2e874b3e2281cae4
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2019-01-22 16:44:09 -0500

    Update roleplay.js

--- roleplay.js
+++ roleplay.js
@@ -66,7 +66,7 @@ let args = message.content.split(/ +/g).slice(1)
    if (!emojis) return message.channel.send("No emoji picked?! Try again.")
 //       let reactie = args.slice(1).join(" ");
 //    if (!reactie) return message.channel.send("No emoji picked?! Try again.")
-  let emojipick = client.emojis.find(emoji => emoji.get(args[0]) === emojis) //emojis.get(args[0])  
+  let emojipick = client.emojis.find(emoji => emoji.find(args[0]) === emojis) //emojis.get(args[0])  
 //    let emojipick = message.guild.emojis.get(reactie)
    if (!emojipick) return message.channel.send("No such emoji is there.")
    let channel = message.guild.channels.find(c => c.id === '534561180811919360');
